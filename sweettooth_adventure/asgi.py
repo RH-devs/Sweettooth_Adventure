76612f5b64b560e4154098537ca862a7bfb93bb1
RH-devs <rasheedatajiborisha@gmail.com>
2022-09-11 14:44:00 +0000

    initial project commit

---
+++ sweettooth_adventure/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sweettooth_adventure project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sweettooth_adventure.settings')
+
+application = get_asgi_application()
